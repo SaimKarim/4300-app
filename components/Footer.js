b6b901842cc39c0f047f314fc8793c2c1c6aa4e6
cgp89673 <119533439+cgp89673@users.noreply.github.com>
2024-04-03 19:10:08 -0400

    Add files via upload

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+const Footer = () => {
+    return (
+      <div className="footer">
+        <h4>Created by:</h4>
+      </div>
+    );
+  };
+  
+  export default Footer;
